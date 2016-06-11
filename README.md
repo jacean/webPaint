3452d51d59afa5c6b864bea985d452e54497f5c8
jacean <jaceanwan@gmail.com>
2016-06-11 13:49:50 +0800

    base function is ok,version1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+#webPaint
+
+webPaint with Zrender
---
+++ uploadimg.php
@@ -0,0 +1,28 @@
+<?php 
+
+$img = $_POST["img"];
+$id = $_POST["id"];
+$list=$_POST["list"];
+$dir = "upload/";
+
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$img = base64_decode($img);
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$file_path = $dir."img_".$id.".png";
+$f = fopen($file_path, "w");
+fwrite($f, $img);
+fclose($f);
+
+$file_path=$dir."list_".$id.".txt";
+$f=fopen($file_path,'w');
+fwrite($f,$list);
+fclose($f);
+
+$r = array("status" => "ok", "result" => $file_path);
+
+echo json_encode($r);
+?>
