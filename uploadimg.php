b548583ee86f30394450963acf384d6fdbac40a7
jacean <jaceanwan@gmail.com>
2016-06-11 18:26:38 +0800

    update 1.Entry.html set iframe within paint;2.set paintAPI to use by jsp or other;3.set icon instead of text

--- uploadimg.php
+++ uploadimg.php
@@ -2,7 +2,6 @@
 
 $img = $_POST["img"];
 $id = $_POST["id"];
-$list=$_POST["list"];
 $dir = "upload/";
 
 $img = str_replace('data:image/png;base64,', '', $img);
@@ -12,15 +11,11 @@ if (!is_dir($dir)) {
     mkdir($dir, 0777, true);
 }
 
-$file_path = $dir."img_".$id.".png";
+$file_path = $dir.$id.".png";
 $f = fopen($file_path, "w");
 fwrite($f, $img);
 fclose($f);
 
-$file_path=$dir."list_".$id.".txt";
-$f=fopen($file_path,'w');
-fwrite($f,$list);
-fclose($f);
 
 $r = array("status" => "ok", "result" => $file_path);
 
